b30deb4812f9b138e4ec67977624bc6c3ba93d77
MaryamM4 <maryamma@outlook.com>
2025-03-16 23:48:44 +0000

    fsFdToFdn helper method for fsRead and fsWrite

--- fs.c
+++ fs.c
@@ -2,19 +2,17 @@
 // fs.c - user FileSytem API
 // ============================================================================
 
-#include "bfs.h"
 #include "fs.h"
+#include "bfs.h"
 
 // ============================================================================
 // Close the file currently open on file descriptor 'fd'.
 // ============================================================================
-i32 fsClose(i32 fd) { 
+i32 fsClose(i32 fd) {
   i32 inum = bfsFdToInum(fd);
   bfsDerefOFT(inum);
-  return 0; 
+  return 0;
 }
-
-
 
 // ============================================================================
 // Create the file called 'fname'.  Overwrite, if it already exsists.
@@ -22,76 +20,84 @@ i32 fsClose(i32 fd) {
 // ============================================================================
 i32 fsCreate(str fname) {
   i32 inum = bfsCreateFile(fname);
-  if (inum == EFNF) return EFNF;
+  if (inum == EFNF)
+    return EFNF;
   return bfsInumToFd(inum);
 }
 
-
-
 // ============================================================================
-// Format the BFS disk by initializing the SuperBlock, Inodes, Directory and 
+// Format the BFS disk by initializing the SuperBlock, Inodes, Directory and
 // Freelist.  On succes, return 0.  On failure, abort
 // ============================================================================
 i32 fsFormat() {
-  FILE* fp = fopen(BFSDISK, "w+b");
-  if (fp == NULL) FATAL(EDISKCREATE);
+  FILE *fp = fopen(BFSDISK, "w+b");
+  if (fp == NULL)
+    FATAL(EDISKCREATE);
 
-  i32 ret = bfsInitSuper(fp);               // initialize Super block
-  if (ret != 0) { fclose(fp); FATAL(ret); }
+  i32 ret = bfsInitSuper(fp); // initialize Super block
+  if (ret != 0) {
+    fclose(fp);
+    FATAL(ret);
+  }
 
-  ret = bfsInitInodes(fp);                  // initialize Inodes block
-  if (ret != 0) { fclose(fp); FATAL(ret); }
+  ret = bfsInitInodes(fp); // initialize Inodes block
+  if (ret != 0) {
+    fclose(fp);
+    FATAL(ret);
+  }
 
-  ret = bfsInitDir(fp);                     // initialize Dir block
-  if (ret != 0) { fclose(fp); FATAL(ret); }
+  ret = bfsInitDir(fp); // initialize Dir block
+  if (ret != 0) {
+    fclose(fp);
+    FATAL(ret);
+  }
 
-  ret = bfsInitFreeList();                  // initialize Freelist
-  if (ret != 0) { fclose(fp); FATAL(ret); }
+  ret = bfsInitFreeList(); // initialize Freelist
+  if (ret != 0) {
+    fclose(fp);
+    FATAL(ret);
+  }
 
   fclose(fp);
   return 0;
 }
-
 
 // ============================================================================
 // Mount the BFS disk.  It must already exist
 // ============================================================================
 i32 fsMount() {
-  FILE* fp = fopen(BFSDISK, "rb");
-  if (fp == NULL) FATAL(ENODISK);           // BFSDISK not found
+  FILE *fp = fopen(BFSDISK, "rb");
+  if (fp == NULL)
+    FATAL(ENODISK); // BFSDISK not found
   fclose(fp);
   return 0;
 }
 
-
-
 // ============================================================================
-// Open the existing file called 'fname'.  On success, return its file 
+// Open the existing file called 'fname'.  On success, return its file
 // descriptor.  On failure, return EFNF
 // ============================================================================
 i32 fsOpen(str fname) {
-  i32 inum = bfsLookupFile(fname);        // lookup 'fname' in Directory
-  if (inum == EFNF) return EFNF;
+  i32 inum = bfsLookupFile(fname); // lookup 'fname' in Directory
+  if (inum == EFNF)
+    return EFNF;
   return bfsInumToFd(inum);
 }
-
-
 
 // ============================================================================
 // Read 'numb' bytes of data from the cursor in the file currently fsOpen'd on
 // File Descriptor 'fd' into 'buf'.  On success, return actual number of bytes
 // read (may be less than 'numb' if we hit EOF).  On failure, abort
 // ============================================================================
-i32 fsRead(i32 fd, i32 numb, void* buf) {
+i32 fsRead(i32 fd, i32 numb, void *buf) {
 
   // ++++++++++++++++++++++++
   // Insert your code here
   // ++++++++++++++++++++++++
 
-  FATAL(ENYI);                                  // Not Yet Implemented!
+  FATAL(ENYI); // Not Yet Implemented!
   return 0;
 }
-
 
 // ============================================================================
 // Move the cursor for the file currently open on File Descriptor 'fd' to the
@@ -105,39 +111,34 @@ i32 fsRead(i32 fd, i32 numb, void* buf) {
 // ============================================================================
 i32 fsSeek(i32 fd, i32 offset, i32 whence) {
 
-  if (offset < 0) FATAL(EBADCURS);
- 
+  if (offset < 0)
+    FATAL(EBADCURS);
+
   i32 inum = bfsFdToInum(fd);
   i32 ofte = bfsFindOFTE(inum);
-  
-  switch(whence) {
-    case SEEK_SET:
-      g_oft[ofte].curs = offset;
-      break;
-    case SEEK_CUR:
-      g_oft[ofte].curs += offset;
-      break;
-    case SEEK_END: {
-        i32 end = fsSize(fd);
-        g_oft[ofte].curs = end + offset;
-        break;
-      }
-    default:
-        FATAL(EBADWHENCE);
+
+  switch (whence) {
+  case SEEK_SET:
+    g_oft[ofte].curs = offset;
+    break;
+  case SEEK_CUR:
+    g_oft[ofte].curs += offset;
+    break;
+  case SEEK_END: {
+    i32 end = fsSize(fd);
+    g_oft[ofte].curs = end + offset;
+    break;
+  }
+  default:
+    FATAL(EBADWHENCE);
   }
   return 0;
 }
 
-
-
 // ============================================================================
 // Return the cursor position for the file open on File Descriptor 'fd'
 // ============================================================================
-i32 fsTell(i32 fd) {
-  return bfsTell(fd);
-}
-
-
+i32 fsTell(i32 fd) { return bfsTell(fd); }
 
 // ============================================================================
 // Retrieve the current file size in bytes.  This depends on the highest offset
@@ -149,19 +150,30 @@ i32 fsSize(i32 fd) {
   return bfsGetSize(inum);
 }
 
-
-
 // ============================================================================
 // Write 'numb' bytes of data from 'buf' into the file currently fsOpen'd on
 // filedescriptor 'fd'.  The write starts at the current file offset for the
 // destination file.  On success, return 0.  On failure, abort
 // ============================================================================
-i32 fsWrite(i32 fd, i32 numb, void* buf) {
+i32 fsWrite(i32 fd, i32 numb, void *buf) {
 
   // ++++++++++++++++++++++++
   // Insert your code here
   // ++++++++++++++++++++++++
 
-  FATAL(ENYI);                                  // Not Yet Implemented!
+  FATAL(ENYI); // Not Yet Implemented!
   return 0;
 }
+
+// ============================================================================
+// Use fs and it's cursor's position to find the corresponding FDN.
+// ============================================================================
+i32 fsFdToFdn(i32 fd) {
+  i32 cursorPos = fsTell(fd);
+  return fsCursorPosToFdn(cursorPos);
+}
+
+// ============================================================================
+// Use the cursor position to find the corresponding FDN.
+// ============================================================================
+i32 fsCursorPosToFdn(i32 cursorPos) { return (i32)(cursorPos / BYTESPERBLOCK); }
